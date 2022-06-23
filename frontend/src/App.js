3f5306dda27ec811f13bfad7a31e03d1f5a894ff
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-23 21:29:52 +0800

    fix panduan page file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,6 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom';
 import Home from './content/home/home';
 import About from './content/about/about';
-import Fakultas from './content/fakultas/Fakultas';
-import Prodi from './content/prodi/prodi';
-import ProdiDetail from './content/prodiDetail/ProdiDetail';
 import Panduan from './content/panduan/Panduan';
 
 const App = () => {
@@ -12,13 +9,6 @@ const App = () => {
 		<Routes>
 			<Route path='/' element={<Home />}/>
 			<Route path='RightWay' element={<About />} />
-			<Route path='fakultas'>
-				<Route index element={<Fakultas />} />
-				<Route path=':name'>
-					<Route index element={<Prodi />} />
-					<Route path=':name' element={<ProdiDetail />} />
-				</Route>
-			</Route>
 			<Route path='panduan'>
 				<Route index element={<Panduan />} />
 			</Route>
