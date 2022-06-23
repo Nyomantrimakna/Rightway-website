88c1460f8e4f5618b9505fbcd1a49b050f2509a0
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-23 21:47:35 +0800

    fix fakultas and prodi file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,21 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom';
 import Home from './content/home/home';
 import About from './content/about/about';
-import Panduan from './content/panduan/Panduan';
+import Fakultas from './content/fakultas/Fakultas';
+import Prodi from './content/prodi/prodi';
+import ProdiDetail from './content/prodiDetail/ProdiDetail';
 
 const App = () => {
 	return(
 		<Routes>
 			<Route path='/' element={<Home />}/>
 			<Route path='RightWay' element={<About />} />
-			<Route path='panduan'>
-				<Route index element={<Panduan />} />
+			<Route path='fakultas'>
+				<Route index element={<Fakultas />} />
+				<Route path=':name'>
+					<Route index element={<Prodi />} />
+					<Route path=':name' element={<ProdiDetail />} />
+				</Route>
 			</Route>
 		</Routes>
 	) 
